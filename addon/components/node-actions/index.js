4449be004439a82aaeba12342e956b9d93362d02
Marcelo Mira <hello@marcemira.com>
2020-08-16 19:38:25 -0300

    Improveing adding/removing with support for configurable node names and op-out ED store

--- addon/components/node-actions/index.js
+++ addon/components/node-actions/index.js
@@ -1,10 +1,17 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { arg } from 'ember-arg-types';
-import { any, func } from 'prop-types';
+import { any, func, string, boolean } from 'prop-types';
+
+const NODE_MODEL_NAME = 'node';
+const NODE_PARENT_NODE_PROPERTY_NAME = 'parentNode';
+const NODE_CHILD_NODE_PROPERTY_NAME = 'childNodes';
 
 export default class NodeActionsComponent extends Component {
+  @service store;
+
   @arg(any.isRequired)
   nodes;
 
@@ -12,10 +19,28 @@ export default class NodeActionsComponent extends Component {
   onSelection;
 
   @arg(func)
+  onBeforeAdd;
+
+  @arg(func)
   onAdd;
 
   @arg(func)
+  onBeforeRemove;
+
+  @arg(func)
   onRemove;
+
+  @arg(boolean)
+  useEDS = true;
+
+  @arg(string)
+  nodeModelName = NODE_MODEL_NAME;
+
+  @arg(string)
+  parentNodeName = NODE_PARENT_NODE_PROPERTY_NAME;
+
+  @arg(string)
+  childNodesName = NODE_CHILD_NODE_PROPERTY_NAME;
 
   @tracked selectedNode;
   @tracked nodeActions;
@@ -66,12 +91,74 @@ export default class NodeActionsComponent extends Component {
   }
 
   @action
-  addNode (node, tree) {
-    debugger;
+  async addNode (parentNode) {
+    let newNode;
+
+    if (this.onBeforeAdd) {
+      newNode = await this.onBeforeAdd(parentNode);
+
+      if (!newNode) {
+        return;
+      }
+    } else {
+      newNode = {
+        name: 'newNode'
+      };
+
+      if (this.useEDS) {
+        newNode = this.store.createRecord(this.nodeModelName, newNode);
+      }
+    }
+
+    parentNode[this.childNodesName].pushObject(newNode);
+
+    if (this.onAdd) {
+      this.onAdd(newNode);
+    }
   }
 
   @action
-  removeNode (node, tree) {
-    debugger;
+  async removeNode (node, tree) {
+    if (this.onBeforeRemove) {
+      const shouldContinue = await this.onBeforeRemove(node);
+
+      if (!shouldContinue) {
+        return;
+      }
+    }
+
+    const parentNode = await node[this.parentNodeName];
+
+    if (parentNode) {
+      if (node[this.childNodesName]) {
+        this._removeChildNodes(node);
+      } else {
+        parentNode[this.childNodesName].removeObject(node);
+      }
+
+      parentNode[this.childNodesName].removeObject(node);
+    }
+
+    if (this.onRemove) {
+      this.onRemove(node);
+    }
+
+    this.selectedNode = null;
+  }
+
+  _removeChildNodes(parentNode) {
+    const childNodes = parentNode[this.childNodesName];
+
+    if (childNodes) {
+      childNodes.forEach(node => {
+        this._removeChildNodes(node);
+
+        parentNode[this.childNodesName].removeObject(node);
+
+        if (this.onRemove) {
+          this.onRemove(node);
+        }
+      });
+    }
   }
 }
