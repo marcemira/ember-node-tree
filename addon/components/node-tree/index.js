7a1e169dd7d2ae1dc5b8adbfe49a143db792240f
Marcelo Mira <hello@marcemira.com>
2020-08-22 17:30:42 -0300

    Calculating node depth for left padding, instead of tree margin gives full width on selection

--- addon/components/node-tree/index.js
+++ addon/components/node-tree/index.js
@@ -1,21 +1,30 @@
 import Component from '@glimmer/component';
 import { set, action } from '@ember/object';
 import { arg } from 'ember-arg-types';
-import { object } from 'prop-types';
+import { object, string } from 'prop-types';
+
+const NODE_PARENT_NODE_PROPERTY_NAME = 'parentNode';
+const NODE_CHILD_NODE_PROPERTY_NAME = 'childNodes';
 
 export default class NodeTreeComponent extends Component {
   @arg(object)
   actionsAPI;
 
+  @arg(string)
+  parentNodeName = NODE_PARENT_NODE_PROPERTY_NAME;
+
+  @arg(string)
+  childNodesName = NODE_CHILD_NODE_PROPERTY_NAME;
+
   @action
   async handleSelection (node) {
-    const parent = await node.parentNode;
+    const parent = await node[this.parentNodeName];
     const nodeSelectedInitialState = node.isSelected;
     const rootNode = parent ? await this._findRoot(parent) : node;
 
-    if (rootNode.childNodes?.length) {
+    if (rootNode[this.childNodesName]?.length) {
       set(rootNode, 'isSelected', false);
-      this._childrenDeselecting(rootNode.childNodes);
+      this._childrenDeselecting(rootNode[this.childNodesName]);
     }
 
     set(node, 'isSelected', !nodeSelectedInitialState);
@@ -26,7 +35,7 @@ export default class NodeTreeComponent extends Component {
   }
 
   async _findRoot(node) {
-    const parentNode = await node.parentNode;
+    const parentNode = await node[this.parentNodeName];
 
     if (parentNode) {
       return this._findRoot(parentNode);
@@ -37,10 +46,12 @@ export default class NodeTreeComponent extends Component {
 
   _childrenDeselecting(nodes) {
     nodes.forEach(node => {
-      set(node, 'isSelected', false);
+      if (!node.isLoading) {
+        set(node, 'isSelected', false);
 
-      if (node.childNodes?.length) {
-        this._childrenDeselecting(node.childNodes);
+        if (node[this.childNodesName]?.length) {
+          this._childrenDeselecting(node[this.childNodesName]);
+        }
       }
     });
   }
--- addon/components/node/index.js
+++ addon/components/node/index.js
@@ -1,10 +1,15 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { set, action } from '@ember/object';
+import { get, set, action } from '@ember/object';
 import { fadeIn, fadeOut } from 'ember-animated/motions/opacity';
 import move from 'ember-animated/motions/move';
 import { arg } from 'ember-arg-types';
-import { object, func } from 'prop-types';
+import { object, string, func } from 'prop-types';
+
+const NODE_PARENT_NODE_PROPERTY_NAME = 'parentNode';
+const NODE_CHILD_NODE_PROPERTY_NAME = 'childNodes';
+const NODE_DEPTH_LEFT_PADDING_AMOUNT = 1;
+const NODE_DEPTH_LEFT_PADDING_UNIT = 'rem'
 
 export default class NodeComponent extends Component {
   @arg(object.isRequired)
@@ -12,6 +17,33 @@ export default class NodeComponent extends Component {
 
   @arg(func)
   onSelection;
+
+  @arg(string)
+  parentNodeName = NODE_PARENT_NODE_PROPERTY_NAME;
+
+  @arg(string)
+  childNodesName = NODE_CHILD_NODE_PROPERTY_NAME;
+
+  get nodeDepth () {
+    let parent = get(this.node, this.parentNodeName);
+    let depth = 0;
+
+    if (parent) {
+      while (get(parent, this.parentNodeName)) {
+        parent = get(parent, this.parentNodeName);
+        depth ++;
+      }
+    }
+
+    return depth;
+  }
+
+  get computedStyle () {
+    const depth = this.nodeDepth;
+    const computedDepth = depth * NODE_DEPTH_LEFT_PADDING_AMOUNT
+
+    return `padding-left: ${computedDepth}${NODE_DEPTH_LEFT_PADDING_UNIT};`;
+  }
 
   @action
   handleClick () {
