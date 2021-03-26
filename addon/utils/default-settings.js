693c90b3e5a7c71a44efeb83b0a5f08c11f212d9
Marcelo Mira <hello@marcemira.com>
2021-03-26 06:35:16 -0300

    Node tree depth padding and style tweaks

--- addon/utils/default-settings.js
+++ addon/utils/default-settings.js
@@ -2,7 +2,7 @@ export const NODE_MODEL_NAME = 'node';
 export const NODE_PARENT_NODE_PROPERTY_NAME = 'parentNode';
 export const NODE_CHILD_NODE_PROPERTY_NAME = 'childNodes';
 export const NODE_DEPTH_LEFT_PADDING_BASE = 4;
-export const NODE_DEPTH_LEFT_PADDING_AMOUNT = 14;
+export const NODE_DEPTH_LEFT_PADDING_AMOUNT = 12;
 export const NODE_DEPTH_LEFT_PADDING_UNIT = 'px';
 
 export const THEME_VARIABLES = {
