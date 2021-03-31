16cb8ef423d2ed9530e4a922ae9912fe2afaf4d3
Marcelo Mira <hello@marcemira.com>
2021-03-31 11:02:07 -0300

    Adding support for the node name component with name autoscrolling

---
+++ addon/components/node-tree/node/name/index.js
@@ -0,0 +1,49 @@
+import Component from '@glimmer/component';
+import {tracked} from '@glimmer/tracking';
+import {action} from '@ember/object';
+import {arg} from 'ember-arg-types';
+import {string} from 'prop-types';
+
+const WORD_SCROLL_DURATION = 0.10;  // Expressed in seconds
+const RIGHT_SCROLL_PADDING = 16;    // Expressed in pixels
+
+export default class NodeTreeNodeNameComponent extends Component {
+  @arg(string)
+  name;
+
+  @tracked nameElement;
+  @tracked shouldScroll = false;
+  @tracked scrollAmount;
+  @tracked scrollDuration;
+
+  @action
+  setupElement (element) {
+    this.nameElement = element.firstElementChild
+    const parentElement = element.parentElement;
+
+    const elementWidth = element.clientWidth;
+    const nameWidth = this.nameElement.clientWidth
+
+    this.shouldScroll = elementWidth < nameWidth;
+    this.scrollAmount = (nameWidth - elementWidth + RIGHT_SCROLL_PADDING) * -1;
+    this.scrollDuration = this.name.length * WORD_SCROLL_DURATION;
+  }
+
+  @action
+  mouseover () {
+    if (this.shouldScroll) {
+      this.nameElement.style.transitionDelay = '0.5s';
+      this.nameElement.style.transitionDuration = `${this.scrollDuration}s`;
+      this.nameElement.style.transform = `translateX(${this.scrollAmount}px)`;
+    }
+  }
+
+  @action
+  mouseout () {
+    if (this.shouldScroll) {
+      this.nameElement.style.transitionDelay = '0s';
+      this.nameElement.style.transitionDuration = '0.5s';
+      this.nameElement.style.transform = `translateX(0)`;
+    }
+  }
+}
