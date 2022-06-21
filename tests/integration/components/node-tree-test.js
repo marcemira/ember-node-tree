fd6f3f48b345f81fbc9bfb36b5e1b27e121e5091
Jeldrik Hanschke <admin@jhanschke.de>
2022-06-21 10:30:21 +0200

    add test that list of nodes is rendered with name

--- tests/integration/components/node-tree-test.js
+++ tests/integration/components/node-tree-test.js
@@ -1,26 +1,31 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'dummy/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { findAll, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import Node from 'dummy/models/node';
 
 module('Integration | Component | node-tree', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders a list of nodes', async function (assert) {
+    this.nodes = [
+      new Node({ name: 'first node' }),
+      new Node({ name: 'second node' }),
+    ];
 
-    await render(hbs`<NodeTree />`);
-
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
     await render(hbs`
-      <NodeTree>
-        template block text
-      </NodeTree>
+      <NodeTree @nodes={{this.nodes}} as |nt|>
+        <nt.Tree />
+      </NodeTree>/>
     `);
-
-    assert.dom(this.element).hasText('template block text');
+    assert.dom('[data-test-node]').exists({ count: 2 }, 'renders all nodes');
+    assert
+      .dom('[data-test-node="first node"]')
+      .hasTextContaining('first node', 'renders name of node');
+    assert.deepEqual(
+      findAll('[data-test-node] .name').map((el) => el.innerText),
+      ['first node', 'second node'],
+      'renders nodes in order of @nodes array'
+    );
   });
 });
